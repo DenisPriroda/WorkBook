8ced078e179ba612f3ab6369b30ea16b216d80ea
admin <admin@DESKTOP-4P9T36J>
2022-05-22 01:42:46 +0300

    Добавьте файлы проекта.

---
+++ WorkBook/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WorkBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(640, 342);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(640, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(348, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(292, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Сохранить в файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(292, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Открыть файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(647, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Дата";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(647, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Сведения";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(650, 69);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 115);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(650, 210);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(251, 83);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(647, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Наименование, дата и номер документа";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(650, 30);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(248, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(650, 324);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 51);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Добавить запись";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(779, 324);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 51);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Редактировать запись";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(713, 381);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 51);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Удалить запись";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(713, 324);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(123, 51);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Подтвердить редактирование";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(713, 405);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(123, 51);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Отменить редактирование";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 468);
+            this.ControlBox = false;
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Электронная трудовая книжка";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ WorkBook/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WorkBook
+{
+    public partial class Form1 : Form
+    {
+
+        class WorkRecord
+        {
+            public string number { get; set; }
+            public string date { get; set; }
+            public string text { get; set; }
+            public string nameDoc { get; set; }
+
+            public WorkRecord(string _number, string _date, string _text, string _nameDoc)
+            {
+
+                number = _number;
+                date = _date;
+                text = _text;
+                nameDoc = _nameDoc;
+            }
+        }
+
+        BindingList<WorkRecord> records;
+        int selected;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button7.Visible = false;
+            button8.Visible = false;
+            records = new BindingList<WorkRecord>();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt";
+            renew_grid();
+        }
+
+        public void renew_grid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = records;
+            dataGridView1.Columns["number"].HeaderText = "№ записи";
+            dataGridView1.Columns["date"].HeaderText = "Дата";
+            dataGridView1.Columns["text"].HeaderText = "Сведения о приеме на работу, квалификации, увольнении";
+            dataGridView1.Columns["nameDoc"].HeaderText = "Наименование, дата и номер документа, на основании которого внесена запись";
+            dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            textBox2.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите выйти? Не забудьте сохранить изменения в файл.", "Выход", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (records.Count > 0)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                string filename = saveFileDialog1.FileName;
+
+                TextWriter wc = new StreamWriter(filename);
+                foreach (WorkRecord rec in records)
+                    wc.WriteLine(rec.number + ";" + rec.date + ";" + rec.text + ";" + rec.nameDoc);
+                wc.Close();
+                MessageBox.Show("Информация сохранена");
+            }
+            else
+            {
+                MessageBox.Show("Записи отсутствуют!", "Сохранение в файл",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string filename = openFileDialog1.FileName;
+            if (File.Exists(filename))
+            {
+                FileStream fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                records.Clear();
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    try
+                    {
+                        string line;
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            String[] words = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                            records.Add(new WorkRecord(words[0], words[1], words[2], words[3]));
+                        }
+                        MessageBox.Show("Информация загружена");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка загрузки файла");
+                        records.Clear();                        
+                    }
+                }
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (records.Count != 0)
+            {
+                if (dataGridView1.CurrentRow.Index > -1)
+                {
+                    records.RemoveAt(dataGridView1.CurrentRow.Index);
+                    renew_grid();
+                }
+                else
+                {
+                    MessageBox.Show("Не выбрано ни единой записи", "Удаление",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Нет записей для удаления", "Удаление",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text != "") && (textBox2.Text != ""))
+            {
+                int number = 0;
+                if (records.Count > 0)
+                {
+                    try
+                    {
+                        number = Int32.Parse(records[records.Count - 1].number);
+                        number++;
+                    }
+                    catch (FormatException ex)
+                    {
+                       MessageBox.Show("Предыдущая запись имела не правильный порядковый номер", "Добавление",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                records.Add(new WorkRecord(number.ToString(), dateTimePicker1.Value.ToShortDateString(), textBox1.Text, textBox2.Text));
+                renew_grid();
+            }
+            else
+            {
+                MessageBox.Show("Сведения или документ не заполнены.", "Добавление",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (records.Count != 0)
+            {
+                if (dataGridView1.CurrentRow.Index > -1)
+                {
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    button5.Visible = false;
+                    button6.Visible = false;
+                    button7.Visible = true;
+                    button8.Visible = true;
+                    textBox1.Text = records[dataGridView1.CurrentRow.Index].text;
+                    textBox2.Text = records[dataGridView1.CurrentRow.Index].nameDoc;
+                    dateTimePicker1.Value = Convert.ToDateTime(records[dataGridView1.CurrentRow.Index].date);
+                    selected = dataGridView1.CurrentRow.Index;
+                }
+                else
+                {
+                    MessageBox.Show("Не выбрано ни единой записи", "Редактирование",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }      
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = false;
+            button8.Visible = false;
+            renew_grid();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text != "") && (textBox2.Text != ""))
+            {
+                records[selected].text = textBox1.Text;
+                records[selected].nameDoc = textBox2.Text;
+                records[selected].date = dateTimePicker1.Value.ToShortDateString();
+                button1.Visible = true;
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+                button7.Visible = false;
+                button8.Visible = false;
+                renew_grid();
+            }
+            else
+            {
+                MessageBox.Show("Сведения или документ не заполнены.", "Редактирование",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
